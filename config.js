35f7d45056f5f83123f55fb18d32d40ded8dd895
trandacconghieu <89311148+trandacconghieu@users.noreply.github.com>
2021-08-21 22:56:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Ngoài kìa trăm ngã lối về. Nếu chưa ai đón thì về với anh😙',
+    desc: 'Đêm nay trời khá nhiều sao. Anh thích em đấy ý em thế nào? ',
     btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnNo: 'Em đồng ý :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
